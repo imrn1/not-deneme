7706af5245b872dfb4a8f028fdf14ba546737fbd
İmran Aykut <49922016+imrn1@users.noreply.github.com>
2021-02-16 20:28:48 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDEqBCH9YtC4MFCLrkBuoyV-GTf6CjqTvk",
-    authDomain: "ang-deneme.firebaseapp.com",
-    projectId: "ang-deneme",
-    storageBucket: "ang-deneme.appspot.com",
-    messagingSenderId: "533670915975",
-    appId: "1:533670915975:web:ede4f6e5b9ce06d492ad22"
+    //****************************//
   }
 };
